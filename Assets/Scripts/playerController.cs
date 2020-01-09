9dd664b39825e8397bc88455e1ba3b21e5e1845c
William <william_fischer@hotmail.de>
2020-01-09 22:30:00 +0100

    Gravity & Walking on Sphere (one planet) Need to expand to all planets

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -9,6 +9,9 @@ public class playerController : MonoBehaviour
     public float moveSpeed = 10.0f;
     private float moveInput;
     public Rigidbody2D rb;
+    public GameObject planet;
+    float maxVelocityChange = 10.0f;
+    float walkspeed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +22,7 @@ public class playerController : MonoBehaviour
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        planet = GameObject.FindGameObjectWithTag("PlanetTest");
     }
 
     // Update is called once per frame
@@ -26,9 +30,32 @@ public class playerController : MonoBehaviour
     {
         if (Input.GetKeyDown("e"))
         {
-            rb.AddForce(new Vector2(0, force), ForceMode2D.Impulse);
+            //rb.AddForce(new Vector2(0, force), ForceMode2D.Impulse);
+            rb.AddForce(transform.up * force, ForceMode2D.Impulse);
         }
-        moveInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        // Calculate how fast we should be moving
+        var targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        targetVelocity = transform.TransformDirection(targetVelocity);
+        targetVelocity *= walkspeed;
+
+        // Apply a force that attempts to reach our target velocity
+        Vector3 velocity = rb.velocity;
+        var velocityChange = (targetVelocity - velocity);
+        velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+        velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+        velocityChange.y = Mathf.Clamp(velocityChange.y, -maxVelocityChange, maxVelocityChange);
+        rb.AddForce(velocityChange, ForceMode2D.Impulse);
+
+
+    }
+
+
+    void Update()
+    {
+        // Orient player upright
+        var down = (planet.transform.position - transform.position).normalized;
+        var forward = Vector3.Cross(transform.right, down);
+        transform.rotation = Quaternion.LookRotation(-forward, -down);
     }
 }
